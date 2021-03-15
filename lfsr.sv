555f6fbded3f81cb84fc63fc0be5a3729876dc16
Basil Mahmood <basilmahmood98@gmail.com>
2021-03-15 16:19:44 -0400

    Changed seed value

--- lfsr.sv
+++ lfsr.sv
@@ -29,7 +29,7 @@ module testbench #(parameter N = 20);
         #clk_period 
         r = 0;
         load = 1;
-        s = 4'b1;
+        s = 4'b0001;
 
         // Finish loading seed
         #clk_period
